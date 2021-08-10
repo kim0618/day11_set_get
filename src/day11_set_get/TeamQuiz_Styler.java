e9ae0e08a39fb1ebd61ab8e47f3a941320db78ee
jinsung <tjd610@naver.com>
2021-08-10 21:12:32 +0900

    t_quiz

---
+++ src/day11_set_get/TeamQuiz_Styler.java
@@ -0,0 +1,31 @@
+package day11_set_get;
+
+class Styler{
+	
+	// 건조, 스팀다림(구김없애기,드라이클리닝), 탈취(먼지제거, 냄세제거)
+	public void steam() {	// 스팀 다림(구김 없애기, 드라이클리닝)
+		
+	}
+	
+	public void remove() { // 탈취(먼지제거, 냄세제거)
+		
+	}
+	
+	public void dry() { // 건조
+		
+	}
+	
+}
+
+public class TeamQuiz_Styler {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/day11_set_get/TeamQuiz_Washing.java
@@ -0,0 +1,42 @@
+package day11_set_get;
+
+class Washing{
+	// 전원, 문닫힘, 무게 감지, 세탁, 코스성정, 동작/일시정지, 탈수/헹금
+	
+	public void power() { // 전원,동작,일시정지
+		
+	}
+	
+	public void closed() { //문 닫힘
+	
+	}
+	
+	public void wei() {  // 무게감지 물온도? 상,중,하
+		
+	}
+	
+	public void washing() { // 세탁,탈수,헹굼
+		
+	}
+	
+	public void course() { //코스설정 - 표준코스, 급속코스, 이불코스
+		
+	}
+	
+}
+
+
+public class TeamQuiz_Washing {
+	public static void main(String[] args) {
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
