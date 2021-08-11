4458f845b5e2e7e0e6b031d97ddb93f1610de0df
jinsung <tjd610@naver.com>
2021-08-11 13:30:20 +0900

    T_Quiz

--- src/day11_set_get/TeamQuiz_Styler.java
+++ src/day11_set_get/TeamQuiz_Styler.java
@@ -1,31 +1,86 @@
 package day11_set_get;
 
+import java.util.Scanner;
+
 class Styler{
 	
-	// 건조, 스팀다림(구김없애기,드라이클리닝), 탈취(먼지제거, 냄세제거)
-	public void steam() {	// 스팀 다림(구김 없애기, 드라이클리닝)
-		
+	Scanner input = new Scanner(System.in);
+	
+	private int stm,re,dr,end,n1;
+	
+	public void steam() {	
+		System.out.println("1. 구김 없애기");
+		System.out.println("2. 드라이 클리닝");
+		System.out.println("3. 나가기");
+		System.out.println(">>> ");
+		stm = input.nextInt();
+		if(stm == 1) {
+			System.out.println("구김 없애기를 시작합니다.");
+		}else if(stm == 2) {
+			System.out.println("드라이 클리닝을 시작합니다.");
+		}else if(stm > 3){
+			System.out.println("없는 기능입니다.");
+		}
 	}
 	
-	public void remove() { // 탈취(먼지제거, 냄세제거)
-		
+	public void remove() { 
+		System.out.println("1. 먼지제거");
+		System.out.println("2. 냄새제거");
+		System.out.println("3. 나가기");;
+		System.out.println(">>> ");
+		re = input.nextInt();
+		if(re == 1) {
+			System.out.println("먼지를 제거합니다.");
+		}else if(re == 2) {
+			System.out.println("냄새를 제거합니다.");
+		}else if(re>3){
+			System.out.println("없는 기능입니다.");
+		}
 	}
 	
-	public void dry() { // 건조
-		
+	public void dry() { 
+		System.out.println("건조를 시작합니다.");
 	}
 	
+	public void pull() {
+		System.out.println("구김 없애기 기능이 시작되었습니다.");
+		System.out.println("드라이 클리닝 기능이 시작되었습니다.");
+		System.out.println("먼지 제거 기능이 시작되었습니다.");
+		System.out.println("냄세 제거 기능이 시작되었습니다.");
+		System.out.println("건조 기능이 시작되었습니다.");
+	}
+	
+	public void main() {
+		while(true) {
+			System.out.println("1. 스팀");
+			System.out.println("2. 탈취");
+			System.out.println("3. 건조");
+			System.out.println("4. 종합기능 시작");;
+			System.out.println(">>> ");
+			n1 = input.nextInt();
+			switch(n1) {
+			case 1:
+				steam();
+				break;
+			case 2:
+				remove();
+				break;
+			case 3:
+				dry();
+				break;
+			case 4:
+				pull();
+				break;
+			}
+		}
+	}
 }
 
 public class TeamQuiz_Styler {
 	public static void main(String[] args) {
 		
-		
-		
-		
-		
-		
-		
-		
+		Styler st = new Styler();
+		st.main();
+
 	}
 }
--- src/day11_set_get/TeamQuiz_Washing.java
+++ src/day11_set_get/TeamQuiz_Washing.java
@@ -1,42 +1,120 @@
 package day11_set_get;
 
+import java.util.Scanner;
+
 class Washing{
-	// 전원, 문닫힘, 무게 감지, 세탁, 코스성정, 동작/일시정지, 탈수/헹금
-	
-	public void power() { // 전원,동작,일시정지
-		
+	Scanner input = new Scanner(System.in);
+	private int po,wa,co,we,clo,n1;
+	public void power() { 
+		System.out.println("1. 전원");
+		System.out.println("2. 동작");
+		System.out.println("3. 일시정지");
+		po = input.nextInt();
+		if(po == 1) {
+			System.out.println("전원이 ON 되었습니다.");
+		}else if(po== 2) {
+			System.out.println("세탁기가 동작합니다.");
+		}else if(po==3) {
+			System.out.println("세탁기가 일시정지 되었습니다.");
+		}else {
+			System.out.println("없는 기능입니다.");
+		}
 	}
 	
-	public void closed() { //문 닫힘
-	
+	public void closed() { 
+		System.out.println("1. 닫힘");
+		System.out.println("2. 열림");
+		clo = input.nextInt();
+		if(clo == 1) {
+			System.out.println("문이 닫힙니다.");
+		}else if(clo == 2) {
+			System.out.println("문이 열립니다.");
+		}
 	}
 	
-	public void wei() {  // 무게감지 물온도? 상,중,하
-		
+	public void wei() {  
+		System.out.println("빨래 무게 설정 : ");
+		System.out.print("1~1000g >>> ");;
+		we = input.nextInt();
+		if(we <300) {
+			System.out.println(we+"g입니다 물높이는 하입니다.");
+			System.out.println("물높이가 설정되었습니다.");
+		}else if(we <600 ) {
+			System.out.println(we+"g입니다 물높이는 중입니다");
+			System.out.println("물높이가 설정되었습니다.");
+		}else if(we <1000) {
+			System.out.println(we+"g입니다 물높이는 상입니다");
+			System.out.println("물높이가 설정되었습니다.");
+		}else {
+			System.out.println("너무 많은 양입니다.");
+		}
 	}
 	
-	public void washing() { // 세탁,탈수,헹굼
-		
+	public void washing() { 
+		System.out.println("1. 세탁");
+		System.out.println("2. 탈수");
+		System.out.println("3. 헹굼");
+		wa = input.nextInt();
+		if(wa == 1) {
+			System.out.println("세탁기능이 추가됩니다.");
+		}else if(wa == 2) {
+			System.out.println("탈수기능이 추가됩니다.");
+		}else if(wa==3) {
+			System.out.println("헹굼기능이 추가됩니다.");
+		}else {
+			System.out.println("없는 기능입니다.");
+		}
 	}
 	
-	public void course() { //코스설정 - 표준코스, 급속코스, 이불코스
-		
+	public void course() { 
+		System.out.println("1. 표준 코스");
+		System.out.println("2. 급속 코스");
+		System.out.println("3. 이불 코스");
+		co = input.nextInt();
+		if(co == 1) {
+			System.out.println("표준 코스를 선택하셨습니다.");
+		}else if(co == 2) {
+			System.out.println("급속 코스를 선택하셨습니다.");
+		}else if(co== 3) {
+			System.out.println("이불 코스를 선택하셨습니다.");
+		}else {
+			System.out.println("없는 기능입니다.");
+		}
+	}
+	
+	public void main() {
+		while(true) {
+			System.out.println("1. 전원");
+			System.out.println("2. 세탁");
+			System.out.println("3. 코스설정");
+			System.out.println("4. 빨래 무게감지");
+			System.out.println("5. 문 닫힘");
+			n1 = input.nextInt();
+			switch(n1) {
+			case 1:
+					power();
+					break;
+			case 2:
+					washing();
+					break;
+			case 3:
+					course();
+					break;
+			case 4:
+					wei();
+					break;
+			case 5:
+					closed();
+					break;
+				}
+			}
 	}
 	
 }
 
-
 public class TeamQuiz_Washing {
 	public static void main(String[] args) {
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
+		Washing ex = new Washing();
+		ex.main();
 	}
 }
